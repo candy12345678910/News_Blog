413352e83c5abbbd79549147b3d1a59bbd6dd154
candy12345678910 <93834243+candy12345678910@users.noreply.github.com>
2024-07-19 01:50:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App=()=>{
+  return(
+    <>
+      <h1 className="font-bold text-3xl text-red-500 underline">Hello World</h1>
+    </>
+  )
+}
+
+export default App;
